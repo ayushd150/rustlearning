6d57db4569133920763c24b8a19f4330596aca0d
ayushd150 <ayushdbpc2005@gmail.com>
2025-08-19 16:31:13 +0530

    updated files

--- readme.md
+++ readme.md
@@ -1,8 +1,21 @@
 rust has 2 steps
 
-build/optimise 
+build/optimise  => cargo build - rust to binary
+
 and then run the file
 
 numbers:-
 
 i8 - i(signed integer) and size [rust is memory safe]
+
+memory management
+
+garbage collector(no dangling pointers, cant do manual memory managaement)
+manual
+rust way
+
+1) Mutability - immutable var represent var whose value cannot be chnaged once assigned
+
+2) stack vs heap
+
+3) ownership - set of rules that govern how rust manage memory
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,27 @@
-fn main() {
-    println!("Hello, world!");
-    let x: i32 = -5;
-    let y: i32 = 1000;
-    let z: f32 = 1000.001;
+// fn main() {
+//     println!("Hello, world!");
+//     let x: i32 = -5;
+//     let y: i 32 = 1000;
+//     let z: f32 = 1000.001;
+// }
 
+
+// fn main(){
+//     let sentence: String = String::from("my name is ayush");
+//     let first_word: String = get_first_word(sentence);
+//     print!("first word: {}", first_word)
+// }
+
+// fn get_first_word(sentence: String) -> String{
+//     for char in sentence.chars() {
+//         if char == ' ' {
+//             break;
+//         }
+//     }
+// }
+
+fn main(){
+    let mut x: String = String::from("hi there");
+    x=2;
+    println!("{}", x)
 }
-
-
