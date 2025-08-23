1c5bf106e6f84c3dd09948837c2728efa2715420
ayushd150 <ayushdbpc2005@gmail.com>
2025-08-23 16:09:19 +0530

    updated main file

--- src/main.rs
+++ src/main.rs
@@ -98,22 +98,22 @@
 
 //23/08
 //vectors
-fn main(){
-    let mut vec = Vec::new();
-    vec.push(1);
-    vec.push(2);
-    println!("{}", vec); //[1,2]
-}
+// fn main(){
+//     let mut vec = Vec::new();
+//     vec.push(1);
+//     vec.push(2);
+//     println!("{}", vec); //[1,2]
+// }
 
-fn even_filter(vec:Vec<i32>) -> Vec<i32> {
-    let mut new_vec = Vec::new();
-    for val in vec{
-        if val % 2==0{
-            new_vec.push(val);
-        }
-    }
-    return new_vec
-}
+// fn even_filter(vec:Vec<i32>) -> Vec<i32> {
+//     let mut new_vec = Vec::new();
+//     for val in vec{
+//         if val % 2==0{
+//             new_vec.push(val);
+//         }
+//     }
+//     return new_vec
+// }
 
 // fn main(){
 //     let numbers = vec![1,2,3]
@@ -122,27 +122,61 @@ fn even_filter(vec:Vec<i32>) -> Vec<i32> {
 
 //hashmaps- stores key value pair
 
-use std::{collection::HashMap, collections::HashMap};
+// use std::{collection::HashMap, collections::HashMap};
 
-fn group_values_by_keys(vec:Vec<(String, i32)>) -> HashMap<String, i32>{
-    let mut hm = HashMap::new();
-    for (key, value) in vec{
-        hm.insert(key, value);
-    }
-    return hm;
-}
+// fn group_values_by_keys(vec:Vec<(String, i32)>) -> HashMap<String, i32>{
+//     let mut hm = HashMap::new();
+//     for (key, value) in vec{
+//         hm.insert(key, value);
+//     }
+//     return hm;
+// }
+
+
+// fn main(){
+//     let input_vec = vec![(String::from("ayush"), 22), (String::from("harish"), 23)];
+//     let hm = group_values_by_keys(input_vec);
+//     println!("{:?}", hm)
+// }
+
+// fn main(){
+//     let nums = vec![1,2,3]
+//     let iter = nums.iter()
+//     for value in iter{
+//         println!("{}", value)
+//     }
+// }
 
 
 fn main(){
-    let input_vec = vec![(String::from("ayush"), 22), (String::from("harish"), 23)];
-    let hm = group_values_by_keys(input_vec);
-    println!("{:?}", hm)
+    let mut v1 = vec![1,2,3]
+    let v1_iter = v1.iter_mut();
+    for val in v1_iter{
+        *val=*val+1 //*val means derefrence */
+    }
+    println!("{:?}", v1) //You’ll get an error because a vector (Vec<i32>) does not implement Display (the trait behind {}). To fix this, Rust provides another placeholder: {:?}
+// It uses the Debug trait, which is meant for debugging / developer output.
 }
 
-fn main(){
-    let nums = vec![1,2,3]
-    let iter = nums.iter()
-    for value in iter{
-        println!("{}", value)
-    }
+//intolter - convert collection to iterator takes ownership of collection
+//iter - if u want immutrable ref to inner var adnd dont want to transfer ownership
+//itermut - mutable ref to inner var and dont want to transfer ownership
+//iterinto - move var to the iterator and dont use if afterwards
+
+//In Rust, an iterator is something that produces items one by one.
+
+// An iterator adaptor is a method that takes one iterator and returns another iterator, usually transforming or filtering the items along the way.
+
+// Importantly: adaptors are lazy → they don’t actually do any work until you call a “consumer” method (like collect, sum, for, etc.).
+
+
+fn main() {
+    let nums = vec![1, 2, 3, 4, 5, 6];
+
+    let result: Vec<i32> = nums.iter()
+                               .filter(|x| *x % 2 == 1) // keep only odd numbers
+                               .map(|x| x * 2)          // double each
+                               .collect();              // turn into new Vec
+
+    println!("{:?}", result); // [2, 6, 10]
 }
